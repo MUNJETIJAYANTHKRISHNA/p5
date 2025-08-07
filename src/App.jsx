17e68c1bb0ea7b4e318554858aa185723704aca7
MUNJETIJAYANTHKRISHNA <2400032115@kluniversity.in>
2025-08-07 07:02:41 +0530

    Update App.jsx with latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const BASEURL = import.meta.env.BASE_URL;
+
     return (
       <>
         <header>
-          <div className='logo'>Grid Demo</div>
+          <div className="logo">Grid Demo</div>
         </header>
+
         <section>
           {[...Array(9)].map((_, i) => (
-            <div className='card' key={i}>
-              <img src={`/p5/${i + 1}.jpeg`} alt={`Image 0${i + 1}`} />
-              <label>Image 0{i + 1}</label>
+            <div className="card" key={i}>
+              <img src={`${BASEURL}${i + 1}.jpeg`} alt={`Image ${i + 1}`} />
+              <label>{`Image ${String(i + 1).padStart(2, '0')}`}</label>
             </div>
           ))}
         </section>
+
         <footer>Copyright © 2025. All rights reserved.</footer>
       </>
     );
